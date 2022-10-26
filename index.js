2f5ca2a01bd851e2cc1ff3a66f74a3a62bc69275
Chandrababu Gowda <96734849+chandrababu-gowda@users.noreply.github.com>
2022-10-26 09:36:57 +0530

    Display the message

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let enteredUsername = document.getElementById("username");
 let enteredPassword = document.getElementById("password");
 let btn = document.getElementById("submit");
+let display = document.getElementById("display");
 
 btn.addEventListener("click", ()=>{
     let username = enteredUsername.value;
@@ -9,6 +10,12 @@ btn.addEventListener("click", ()=>{
     let usernameCondition = username.includes("@gmail.com");
     let passwordCondition = password.length > 8 && password.length < 32;
 
+    if (!usernameCondition || !passwordCondition) {
+        display.innerHTML = `<h3>Username or password is incorrect</h3>`;
+        // console.log("Username or password is incorrect");
+    }else{
+        display.innerHTML = ` `
+    }
     // console.log(password.length);
 
     // console.log(username);
